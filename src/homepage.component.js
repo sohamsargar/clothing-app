aff6ef8c9bd5ed4da0f09df843dc9fd52a41b84c
sohamhefty <soham@houseofgaming.in>
2023-03-28 12:42:17 +0530

    created project

---
+++ src/homepage.component.js
@@ -0,0 +1,40 @@
+import React from "react";
+const HomePage = () => {
+  return (
+    <section className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span className="subtitle">Shop now</span>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Sneakers</h1>
+              <span className="subtitle">Shop now</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Men</h1>
+              <span className="subtitle">Shop now</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">women</h1>
+              <span className="subtitle">Shop now</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default HomePage;
