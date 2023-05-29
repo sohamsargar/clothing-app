30036a0753cc2ec7aa74450c1cf45599df9711b6
sohamhefty <sohamsargar55@gmail.com>
2023-05-29 16:37:08 +0530

    d

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
   appId: "1:396338393923:web:f3232475979a10db5ab5f6",
   measurementId: "G-DLK77PLBG9",
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      //creating data
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+  // console.log(snapShot);
+};
 
 firebase.initializeApp(config);
 
