9300a46c959f8921fc4c61cc9c497f639d1a274f
sohamhefty <sohamsargar55@gmail.com>
2023-06-06 17:58:52 +0530

    cart icon

--- src/App.js
+++ src/App.js
@@ -2,40 +2,18 @@ import "./App.css";
 // import { Route, Routes } from "react-router-dom";
 // import HomePage from "./components/pages/homepage/homepage.component";
 import Route1 from "./routing";
-import { auth } from "./firebase/firebase.utils";
-import React from "react";
 
 // const Hatspage = () => <div>Hatspage</div>;
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
-  unsubscriberFromAuth = null;
-
-  componentDidMount() {
-    this.unsubscriberFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    });
-  }
-  componentWillUnmount() {
-    this.unsubscriberFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        {/* <Routes>
+function App() {
+  return (
+    <div>
+      {/* <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/hats" Component={Hatspage} />
       </Routes> */}
-        <Route1 currentUser={this.state.currentUser} />
-      </div>
-    );
-  }
+      <Route1 />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/header.component.js
+++ src/components/Header/header.component.js
@@ -4,7 +4,9 @@ import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/084 crown.svg";
 import { auth } from "../../firebase/firebase.utils";
-const Header = ({ currentUser }) => {
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/">
@@ -26,12 +28,15 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
